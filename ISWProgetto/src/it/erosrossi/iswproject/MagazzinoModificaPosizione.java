ccd5f7710c0f90bcbcb282cbd23ebd73e56f548c
Eros Rossi <possi1996.er@gmail.com>
2018-06-20 17:32:53 +0200

    Manca implementazione della modifica del file

--- ISWProgetto/src/it/erosrossi/iswproject/MagazzinoModificaPosizione.java
+++ ISWProgetto/src/it/erosrossi/iswproject/MagazzinoModificaPosizione.java
@@ -7,7 +7,12 @@ import java.awt.event.MouseEvent;
 import java.io.IOException;
 
 public class MagazzinoModificaPosizione extends JFrame {
-    
+    // Bottone per il ritorno alla home
+    private static final JButton home = new JButton("Home");
+    // Bottone per il ritorno alla home
+    private static final JButton aggiorna = new JButton("Aggiorna");
+
+    // Da implementare metodo di ricerca su file e modifica posizione.
 
     public MagazzinoModificaPosizione(  )
     {
@@ -19,6 +24,69 @@ public class MagazzinoModificaPosizione extends JFrame {
         GridBagLayout gridbag = new GridBagLayout();
         magazzinoModPosizioneContainer.setLayout(gridbag);
         GridBagConstraints c = new GridBagConstraints();
+
+        // Testo per id oggetto
+        JLabel testoID = new JLabel("Inserisci codice oggetto: ");
+        c.gridx = 0;
+        c.gridy = 0;
+        c.anchor = GridBagConstraints.LINE_START;
+        gridbag.setConstraints( testoID, c );
+        magazzinoModPosizioneContainer.add( testoID );
+
+        // Casella di inserimento codice oggetto
+        final JTextField codice = new JTextField(10); // Casella di testo
+        c.gridx = 1;
+        c.gridy = 0;
+        gridbag.setConstraints(codice, c);
+        magazzinoModPosizioneContainer.add(codice);
+
+        // Testo nuova posizione
+        JLabel testoPosizione = new JLabel("Inserisci la nuova posizione: ");
+        c.gridx = 0;
+        c.gridy = 1;
+        c.anchor = GridBagConstraints.LINE_START;
+        gridbag.setConstraints( testoPosizione, c );
+        magazzinoModPosizioneContainer.add( testoPosizione );
+
+        // Casella di inserimento nuova posizione
+        final JTextField nuovaPosizione = new JTextField(10); // Casella di testo
+        c.gridx = 1;
+        c.gridy = 1;
+        gridbag.setConstraints(nuovaPosizione, c);
+        magazzinoModPosizioneContainer.add(nuovaPosizione);
+
+        //Inserimento riga vuota
+        JLabel testoVuoto = new JLabel(" ");
+        c.gridx = 0;
+        c.gridy = 2;
+        gridbag.setConstraints( testoVuoto, c );
+        magazzinoModPosizioneContainer.add( testoVuoto );
+
+        // Tasto per aggiornare la posizione
+        c.gridx = 0;
+        c.gridy = 3;
+        gridbag.setConstraints(aggiorna, c);
+        magazzinoModPosizioneContainer.add(aggiorna);
+        aggiorna.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                //Codice da inserire
+            }
+        });
+
+        c.gridx = 1;
+        c.gridy = 3;
+        gridbag.setConstraints(home, c);
+        magazzinoModPosizioneContainer.add(home);
+        home.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                new LoginWindow();
+                modificaPosizione.setVisible(false);
+            }
+        });
+
+        modificaPosizione.setVisible(true);
 
     }
 }
