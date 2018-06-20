0654979b1ad935851e471b0f10d9cee7b21cca35
Eros Rossi <possi1996.er@gmail.com>
2018-06-20 15:59:31 +0200

    Finestra di login del magazzino - Completata

--- ISWProgetto/src/it/erosrossi/iswproject/MagazzinoWindow.java
+++ ISWProgetto/src/it/erosrossi/iswproject/MagazzinoWindow.java
@@ -7,27 +7,71 @@ import java.awt.event.MouseEvent;
 import java.io.IOException;
 
 public class MagazzinoWindow extends JFrame {
-    private static final JButton home = new JButton("home");
+    // Bottone per il ritorno alla home
+    private static final JButton home = new JButton("Home");
+    // Bottone per la visualizzazione degli ordini
+    private static final JButton ordini = new JButton("Controllo Ordini");
+    // Bottone per la visualizzazione degli ordini
+    private static final JButton modificaPosizione = new JButton("Modifica Disposizione");
 
 
     public MagazzinoWindow(  )
     {
 
-        final JFrame magazzino = new JFrame("Magazzino"); // Creo la finestra per l'autenticazione.
+        final JFrame magazzino = new JFrame("Magazzino"); // Inizializzo la finestra con un titolo.
         magazzino.setSize(600, 500);
         magazzino.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container magazzinoContainer = magazzino.getContentPane();
         GridBagLayout gridbag = new GridBagLayout();
         magazzinoContainer.setLayout(gridbag);
         GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.HORIZONTAL; // Non so come mai ma lasciamolo.
+        //c.fill = GridBagConstraints.HORIZONTAL; // Non so come mai ma lasciamolo.
 
+        // Inserimento del testo iniziale.
+        JLabel testoMagazzino = new JLabel("Seleziona quale operazione eseguire: ");
+        c.gridx = 0;
+        c.gridy = 0;
+        c.anchor = GridBagConstraints.CENTER;
+        gridbag.setConstraints( testoMagazzino, c );
+        magazzinoContainer.add( testoMagazzino );
 
-        magazzinoContainer.add(new JLabel("benvenuto nella sezione dedicata al magazzino"));
-        magazzino.setVisible(true);
-
-        c.gridx = 2;
+        // Inserimento riga vuota
+        JLabel testoVuoto = new JLabel(" ");
+        c.gridx = 0;
         c.gridy = 1;
+        gridbag.setConstraints( testoVuoto, c );
+        magazzinoContainer.add( testoVuoto );
+
+        // Tasto per gestione ordini
+        c.gridx = 0;
+        c.gridy = 3;
+        c.anchor = GridBagConstraints.LINE_END;
+        gridbag.setConstraints(ordini, c);
+        magazzinoContainer.add(ordini);
+        ordini.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                new LoginWindow();
+                magazzino.setVisible(false);
+            }
+        });
+
+        // Tasto per modifica posizione
+        c.gridx = 1;
+        c.gridy = 3;
+        gridbag.setConstraints(modificaPosizione, c);
+        magazzinoContainer.add(modificaPosizione);
+        modificaPosizione.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                new LoginWindow();
+                magazzino.setVisible(false);
+            }
+        });
+
+        // Tasto per tornare alla home
+        c.gridx = 2;
+        c.gridy = 3;
         gridbag.setConstraints(home, c);
         magazzinoContainer.add(home);
         home.addMouseListener(new MouseAdapter() {
@@ -37,6 +81,8 @@ public class MagazzinoWindow extends JFrame {
                 magazzino.setVisible(false);
                 }
             });
+
+        magazzino.setVisible(true); // Rendo la finestra visibile, sempre mettere in fondo al metodo.
 
     }
 }
