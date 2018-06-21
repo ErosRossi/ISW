684aff7a792b60f57810ff977696f22da7749b44
Eros Rossi <possi1996.er@gmail.com>
2018-06-21 11:52:43 +0200

    Implementata gestione popup da inserimento articolo

--- ISWProgetto/src/it/erosrossi/iswproject/PopUpWindow.java
+++ ISWProgetto/src/it/erosrossi/iswproject/PopUpWindow.java
@@ -9,6 +9,8 @@ import java.util.concurrent.TimeUnit;
 
 public class PopUpWindow extends JFrame {
     private static final JButton home = new JButton("Home"); // Bottone per la home
+    private static final JButton inserimentoArticolo = new JButton("Inserimento Articolo"); // Bottone per il ritorno all'inserimento dell'articolo
+
 
     public PopUpWindow(  int x ) {
 
@@ -23,7 +25,7 @@ public class PopUpWindow extends JFrame {
 
         switch ( x ) // Switch case per decidere cosa stampare in caso di errore; ricordarsi ogni volta di nominare in modo diverso la variabile del testo.
         {
-            case 0:
+            case 0: // Caso di credenziali di accesso di LoginWindow errrate; ritorno alla LoginWindow.
                 JLabel testoErCredenziali = new JLabel( "Credenziali di accesso non valide!");
                 c.gridx = 0;
                 c.gridy = 0;
@@ -44,8 +46,30 @@ public class PopUpWindow extends JFrame {
 
                 break;
 
-        }
 
+
+            case 1: // Caso di campi non tutti completati quando si inserisce un articolo.
+                JLabel testoErCampiInserimento = new JLabel( "Non hai compilato tutti i campi!");
+                c.gridx = 0;
+                c.gridy = 0;
+                gridbag.setConstraints( testoErCampiInserimento , c );
+                popupContainer.add( testoErCampiInserimento );
+
+                c.gridx = 0;
+                c.gridy = 1;
+                gridbag.setConstraints(inserimentoArticolo, c);
+                popupContainer.add(inserimentoArticolo);
+                inserimentoArticolo.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        new InserimentoArticoliSegreteria();
+                        popup.setVisible(false);
+                    }
+                });
+
+                break;
+
+        }
 
         popup.setVisible(true);
     }
